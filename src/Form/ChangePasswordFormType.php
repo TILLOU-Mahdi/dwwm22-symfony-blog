41876d78fc7bd2f19b3e3f26514393b2e7a95c61
Mahdi T <mahdi-tillou@hotmail.com>
2026-05-15 01:17:51 +0200

    edit: mdp oublié

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
@@ -16,36 +15,40 @@ class ChangePasswordFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('currentPassword', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir votre mot de passe actuel.',
+                    ]),
+                ],
+            ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'first_options' => [
-                    'constraints' => [
-                        new NotBlank([
-                            'message' => 'Le mot de passe est obligatoire',
-                        ]),
-                        new Length([
-                            'min' => 12,
-                            'max' => 255,
-                            'minMessage' => 'Le mot de passe de passe doit au minimum contenir {{ limit }} caractères.',
-                            'maxMessage' => 'Le mot de passe de passe doit au minimum contenir {{ limit }} caractères.',
-                        ]),
-                        new Regex([
-                            'pattern' => '/^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ỳ])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ỳ0-9]).{11,255}$/',
-                            'match' => true,
-                            'message' => "Le mot de passe doit être composé d'au moins une lettre majuscule et miniscule, d'un ciffre et d'un caractère spécial.",
-                        ]),
-                    ],
-                ],
-                'invalid_message' => 'Le mot de passe doit être identique à sa confirmation.',
-                // Instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
-            ])
-        ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([]);
+                'invalid_message' => 'Les deux mots de passe doivent être identiques.',
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le nouveau mot de passe',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nouveau mot de passe est obligatoire.',
+                    ]),
+                    new Length([
+                        'min' => 12,
+                        'max' => 255,
+                        'minMessage' => 'Le mot de passe doit contenir au minimum {{ limit }} caractères.',
+                        'maxMessage' => 'Le mot de passe doit contenir au maximum {{ limit }} caractères.',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ỳ])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ỳ0-9]).{12,255}$/',
+                        'message' => 'Le mot de passe doit contenir au moins une minuscule, une majuscule, un chiffre et un caractère spécial.',
+                    ]),
+                ],
+            ]);
     }
 }
